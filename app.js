2cc0aa9377f583ae4c78a14181c7331399142f10
Rafael Ribeiro <rafaelribdev@gmail.com>
2023-08-23 23:52:15 -0300

    rota de postagens

--- app.js
+++ app.js
@@ -6,8 +6,25 @@
     const admin = require('./routes/admin');
     const path = require('path');
     const mongoose = require('mongoose');
+    const session = require('express-session');
+    const flash = require('connect-flash');
 
 // Configurações
+    // Sessão
+        app.use(session({
+            secret: "cursoDeNode",
+            resave: true,
+            saveUninitialized: true
+        }));
+        app.use(flash());
+
+    // Middleware
+        app.use((req, res, next) => {
+            res.locals.success_msg = req.flash("success_msg");
+            res.locals.error_msg = req.flash("error_msg");
+            next(); 
+        });
+
     // Body Parser
         app.use(express.urlencoded({extended: true}));
         app.use(bodyParser.json());
@@ -47,4 +64,4 @@
 const PORT = 8081;
 app.listen(PORT, () => {
     console.log("Servidor rodando! Port:" + PORT);
-});
+}); 
--- routes/admin.js
+++ routes/admin.js
@@ -15,28 +15,129 @@ router.get('/posts', (req, res) => {
     res.send("Página de posts!");
 });
 
-router.get('/categorias', (req, res) => {
-    res.render('admin/categorias');
-});
+/* ROTA ADMIN/CATEGORIAS */
+    router.get('/categorias', (req, res) => {
+        Categoria.find()
+                 .sort({date: 'desc'})   
+                 .then((categorias) => {
+                    res.render('admin/categorias', {categorias: categorias});
+                 })   
+                 .catch((error) => {
+                    req.flash("error_msg", "Houve um erro ao listar as categorias");
+                    res.redirect('/admin');
+                 });
+    });
 
 router.get('/categorias/add', (req, res) => {
     res.render('admin/addcategorias');
 });
 
-router.post('/categorias/nova', (req, res) => {
-    const novaCategoria = {
-        nome: req.body.name,
-        slug: req.body.slug
-    };
+/* ROTA NOVA CATEGORIA */
+    router.post('/categorias/nova', (req, res) => {
+        
+        var erros = [];
 
-    new Categoria(novaCategoria)
-        .save()
-        .then(() => {
-            console.log("Sucesso no cadastro de dados no BD!");
-        })
-        .catch((error) => {
-            console.log("Erro ao salvar dados no BD: " + error);
-        });
+        if(!req.body.name || typeof req.body.name == undefined || req.body.name == null){
+            erros.push({texto: "Nome inválido"});
+        }
+
+        if(req.body.name.length < 2){
+            erros.push({texto: "Nome da categoria muito pequeno"});
+        }
+
+        if(erros.length > 0){
+            res.render("admin/addcategorias", {erros: erros});
+        } else {
+            const novaCategoria = {
+                nome: req.body.name,
+                slug: req.body.slug
+            };
+        
+            new Categoria(novaCategoria)
+                .save()
+                .then(() => {
+                    req.flash("success_msg", "Categoria criada com sucesso!");
+                    res.redirect('/admin/categorias');
+                })
+                .catch((error) => {
+                    req.flash("error_msg", "Erro ao criar categoria, tente novamente");
+                    res.redirect('/admin');
+                });
+        }
+    });
+
+/* EDIÇÃO DE CATEGORIA */
+    router.get('/categorias/edit/:id', (req, res) => {
+        Categoria.findOne({_id: req.params.id})
+                 .then((categoria) => {
+                    res.render('admin/editcategorias', {categoria: categoria});
+                 })
+                 .catch((error) => {
+                    req.flash("error_msg", "Essa categoria não existe!");
+                    res.redirect('/admin/categorias');
+                 });
+    });
+
+/* APLICANDO EDIÇÃO DE CATEGORIA - POST */
+    router.post('/categorias/edit', (req, res) => {
+        Categoria.findOne({_id: req.body.id})
+                 .then((categoria) => {
+                    categoria.nome = req.body.name;
+                    categoria.slug = req.body.slug;
+
+                    categoria.save()
+                             .then(() => {
+                                req.flash("success_msg", "Categoria editada com sucesso!");
+                                res.redirect("/admin/categorias");
+                             })
+                             .catch((error) => {
+                                req.flash("error_msg", "Erro ao salvar categoria editada!");
+                                res.redirect("/admin/categoria");
+                             });
+                 })
+                 .catch((error) => {
+                    console.log(error);
+                    req.flash("error_msg", "Houve um erro ao editar a categoria: " + error);
+                    res.redirect("/admin/categorias");
+                 });
+    });
+
+/* DELETAR CATEGORIA */
+router.post('/categorias/deletar', (req, res) => {
+    Categoria.deleteOne({_id: req.body.id})
+            .then(() => {
+                req.flash("success_msg", "Categoria deletada com sucesso!");
+                res.redirect('/admin/categorias');
+            })
+            .catch((error) => {
+                req.flash("error_msg", "Erro ao deletar categoria: " + error);
+                res.redirect('/admin/categorias');
+            });
 });
+
+/* ROTAS DE POSTAGENS */
+router.get('/postagens', (req, res) => {
+    res.render('admin/postagens');
+});
+
+/* ADICIONANDO NOVA POSTAGEM */
+router.get('/postagens/add', (req, res) => {
+
+    /* Passa todas as categorias que existe no banco de dados
+    para a nossa view de postagens (nova postagem) */
+        Categoria.find()
+                .then((categorias) => {
+                    res.render('admin/addpostagem', {categorias: categorias});
+                })
+                .catch((error) => {
+                    req.flash("error_msg", "Erro ao criar categoria: " + error);
+                    res.rendirect('/admin/postagens');
+                });
+});
+
+
+
+
+
 
 module.exports = router;
